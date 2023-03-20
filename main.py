dc591b94a32ac65d2ef29539c57b6c64ae2ecb42
Dominik Gumienny <dominik.gumienny00@gmail.com>
2023-03-20 12:27:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,28 +3,32 @@ import pyautogui
 import time
 import keyboard
 import random
-import win32api, win32con
+import win32api
+import win32con
 
+
+# Coordinates of black tiles
 # 601, 650
 # 688, 650
 # 772, 650
 # 861, 650
 
+
 def click(x, y):
-    win32api.SetCursorPos((x,y))
-    # Click then release
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.05)  # This pauses the script for 0.05 seconds
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
 
 while keyboard.is_pressed('q') == False:
 
     # Check each tile
-    if pyautogui.pixel(601, 650) [0] == 0:
-        click(601, 650)
-    if pyautogui.pixel(688, 650) [0] == 0:
-        click(688, 650)
-    if pyautogui.pixel(772, 650) [0] == 0:
-        click(772, 650)
-    if pyautogui.pixel(861, 650) [0] == 0:
-        click(861, 650)
+    if pyautogui.pixel(601, 420)[0] == 0:
+        click(601, 420)
+    if pyautogui.pixel(688, 420)[0] == 0:
+        click(688, 420)
+    if pyautogui.pixel(772, 420)[0] == 0:
+        click(772, 420)
+    if pyautogui.pixel(861, 420)[0] == 0:
+        click(861, 420)
